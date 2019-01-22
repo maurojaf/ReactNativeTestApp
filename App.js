6b40d0cb949b176e8660d90d1f7505f11d011eed
mauricio <mauricio.atenas@cademsmart.cl>
2019-01-22 18:32:15 -0300

    Clase 23
    BackGround Image, Buttons y Preloader OK!

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import {StyleSheet, Text, View  } from 'react-native';
-
+import AppButton from './application/components/AppButton';
+import Preloader  from './application/components/PreLoader';
+import BackgroundImage  from './application/components/BackgroundImage';
 
 export default class App extends React.Component {
   render() {    
       return (
-        <View style={styles.container}>
-            <Text>Testeando App!!!!</Text>
-        </View>
+          <BackgroundImage
+            source={require('./assets/images/background1.png')}>
+            <Text style={{color:'#fff', marginTop: 100, textAlign: 'center'}}>Hola</Text>
+          </BackgroundImage>
+
+        // <Preloader/>
+        // <View style={styles.container}>
+        //     {/* <AppButton
+        //       bgColor="rgba(111, 38, 74, 0.7)"
+        //       title="SIGN IN"
+        //       action={() => console.log(1)}
+        //       iconName="sign-in"
+        //       iconSize={30}
+        //       iconColor="#fff"
+        //     /> */}
+        // </View>
       );
     } 
   }
